fc9bc977013cbb51425275b5321ca1e1a49cf018
josephs <josephs@ci.garden-grove.ca.us>
2019-06-05 10:30:14 -0700

    initial commit

---
+++ js/L.Control.Search.js
@@ -0,0 +1,74 @@
+L.Control.Search = L.Control.extend({
+  options: {
+    position: 'topright'
+  },
+
+  onAdd: function (map) {
+    this._map = map;
+    this._popup  = L.popup({ autoPan: true, maxWidth: 200 }).setContent('Start searching above!');
+
+    this._container = L.DomUtil.create('div', 'search-control');
+    this._form = L.DomUtil.create('form', 'search-form', this._container);
+
+    this._form.innerHTML = '<div class="input-group">' +
+      '<input id="address-search" class="form-control form-control-lg" type="search" placeholder="eg. 11222 Acacia Pkwy" />' +
+      '<div class="input-group-append"><button class="btn btn-primary btn-lg" type="submit">&raquo;</button></div>' +
+      '</div>';
+
+    L.DomEvent.on(this._form, 'submit', this._search, this);
+
+    L.DomEvent.disableClickPropagation(this._container);
+
+    return this._container;
+  },
+
+  _search: function (e) {
+    L.DomEvent.stop(e);
+
+    var searchUrl = (this.options.searchUrl) ? this.options.searchUrl : '/maps/api/addresses/info';
+    
+    if ($('#address-search').val() == '') return;
+
+    if (screen.width < 768) {
+      $('#address-search').blur();
+    } else {
+      $('#address-search').select();
+    }
+    $.get(
+      searchUrl, 
+      { q: $('#address-search').val() },
+      $.proxy(this._process_results, this)
+    );
+  },
+
+  _process_results: function (data) {
+    if (data.latitude === undefined) {
+      alert("We weren't able to find your address. Please try again or give us a call!");
+      return;
+    }
+
+    var popupContent = '<div style="text-align: center; font-size: 1.1em;">' + data.address + '</div>';
+    if (this.options.popupContent) {
+      if (typeof this.options.popupContent === 'function') {
+        popupContent = this.options.popupContent.call(null, data);
+      }
+    }
+
+    var latlng = L.latLng(data.latitude, data.longitude);
+
+    if (this._marker === undefined) {
+      this._marker = L.marker(latlng);
+      this._marker.addTo(this._map);
+      this._marker.bindPopup(this._popup);
+    }
+
+    this._popup.setContent(popupContent);
+    this._marker.setLatLng(latlng).openPopup();
+
+    this._map.setView(latlng, 18);
+  }
+});
+
+L.control.search = function (options) {
+  return new L.Control.Search(options);
+}
---
+++ js/lh.js
@@ -0,0 +1,129 @@
+require.config({
+  baseUrl: 'https://ggcity.org/maps/js'
+});
+
+require([
+  "https://unpkg.com/leaflet@1.5.1/dist/leaflet.js",
+  "https://code.jquery.com/jquery-3.4.1.min.js"
+], function () { require([
+  "leaflet.edgebuffer",
+  "leaflet.wms",
+  "L.Control.Search",
+  "https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js",
+  "https://cdn.maptiks.com/maptiks-leaflet.min.js"
+], function () {
+  maptiks.trackcode='7582471a-da37-44e3-8dd1-dbc7bc3f8ca2';
+
+  var Lh = {};
+  
+  Lh.map = function (mapDiv, options) {
+    var map = L.map(mapDiv, $.extend({
+      bounds: [[33.732480,-118.042790], [33.805686, -117.894278]],
+      fullscreenControl: true,
+      scrollWheelZoom: true,
+      dragging: true,
+      zoomControl: true,
+      doubleClickZoom: true,
+      touchZoom: true,
+      attributionControl: false,
+      keyboard: true,
+      inertiaDeceleration: 3000,
+      inertiaMaxSpeed: 3000,
+      maxBounds: [[32,-121], [36.5, -113.5]],
+      maxBoundsViscosity: 0.1,
+      minZoom:12,
+      maxZoom:20
+    }, options));
+
+    map.fitBounds(map.options.bounds);
+
+    L.control.scale().addTo(map);
+    L.control.attribution({ prefix: '<a href="https://ggcity.org/maps" target="_blank">GGCity</a> ' }).addTo(map);
+  
+    return map;
+  };
+
+  // load basemap from ggcity Mapbox account
+  Lh.mapbox = function (style) {
+    // username, styles, and public-maps token from ggcity Mapbox account
+    var userName = 'ggcity';
+    // token only allows access from ggcity.org, ch.ci.garden-grove.ca.us
+    var accessToken = 'pk.eyJ1IjoiZ2djaXR5IiwiYSI6ImNqd2lkM3N2dDAza2Y0YW43MHU2dHVlczcifQ.xRMhGcP2VXTdJ408o4cJnA';
+   
+    var styles = {
+      'boundary': 'cjwibofeg5eup1cp9h6zy34m7',
+      'no-boundary': 'cjwibptg006up1dlu7z2yn8ve'
+    };
+
+    if (styles[style] === undefined) console.error('Specified style not defined');
+
+    // concat the url
+    var basemapURL = 'https://api.mapbox.com/styles/v1/' + userName + '/' + styles[style] + '/tiles/256/{z}/{x}/{y}?access_token=' + accessToken;
+    return Lh.tileLayer(basemapURL);
+  };
+  
+  // load a basemap from a specified url with options. defaults to options below
+  Lh.tileLayer = function (url, options) {
+    return L.tileLayer(url, $.extend({
+      attribution: '&copy; <a href="http://openmaptiles.org/" target="_blank">OpenMapTiles</a>' +
+        ' &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors' +
+        ' &copy; <a href="https://github.com/microsoft/USBuildingFootprints/blob/master/LICENSE" target="_blank">Microsoft</a>',
+      edgeBufferTiles: 2,
+      maxZoom: 20
+    }, options));
+  };
+  
+  Lh.wms = function (url, layers, options) {
+    var source = L.WMS.source(url, $.extend({
+      transparent: true,
+      format: 'image/png',
+      info_format: 'text/html',
+      identify: true,
+      maxZoom:20
+    }, options));
+    
+    var wmsLayers;
+    if (typeof layers === 'string') {
+      // return single wms
+      wmsLayers = source.getLayer(layers);
+    } else if (Array.isArray(layers)) {
+      // return array of wms
+      var wmsLayers = [];
+      for (var i = 0; i < layers.length; i++) {
+        wmsLayers.push(source.getLayer(layers[i]));
+      }
+    } else if (typeof layers === 'object') {
+      // return object of named layers for adding TOC
+      wmsLayers = {};
+      for (key in layers) {
+        wmsLayers[key] = source.getLayer(layers[key]);
+      }
+    }
+    return wmsLayers;
+  };
+
+  Lh.aerial = function () {
+    return L.tileLayer.wms('//ggcity.org/geoserver/gwc/service/wms', {
+      layers: 'gis:aerials',
+      format: 'image/png',
+      transparent: false,
+      maxZoom: 20,
+      maxNativeZoom: 20
+    });
+  };
+
+  main(Lh);
+})});
+
+function loadCss(url) {
+  var link = document.createElement("link");
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.href = url;
+  document.getElementsByTagName("head")[0].appendChild(link);
+}
+
+loadCss("https://unpkg.com/leaflet@1.5.1/dist/leaflet.css");
+loadCss("https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css");
+loadCss("https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/leaflet.fullscreen.css");
+loadCss("/maps/css/main.css");
